2ecce5866102d51f0df16100b9d80b751982e430
Mateo <mateozubieta13@gmail.com>
2026-09-16 13:49:57 -0400

    cambios en las fuentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  // applyBaseStyles: false porque global.css ya incluye las directivas @tailwind
+  integrations: [tailwind({ applyBaseStyles: false }), react()],
   site: "https://mattzub13.github.io",
   base: "/",
 });
